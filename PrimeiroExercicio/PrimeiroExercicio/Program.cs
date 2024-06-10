52120d85797632e885ed7f0aea060972983ba86c
Euller Pastro <eullerpastro0@gmail.com>
2024-06-10 00:38:21 -0300

    Primeiro exercicio

---
+++ PrimeiroExercicio/PrimeiroExercicio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrimeiroExercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triangulo x:");
+            xA = double.Parse(Console.ReadLine());
+            xB = double.Parse(Console.ReadLine());
+            xC = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do triangulo y:");
+            yA = double.Parse(Console.ReadLine());
+            yB = double.Parse(Console.ReadLine());
+            yC = double.Parse(Console.ReadLine());
+
+            double p = (xA + xB + xC) / 2.0;
+            double areax = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areay = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Area de x = " + areax.ToString("F4"));
+            Console.WriteLine("Area de y = " + areay.ToString("F4"));
+
+            if (areax > areay)
+            {
+                Console.WriteLine("Maior area: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior area: Y");
+            }
+        }
+    }
+}
