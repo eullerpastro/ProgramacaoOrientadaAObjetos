f8ca606bbc44f4f58e4cc3132106fddc85f58302
Euller Pastro <eullerpastro0@gmail.com>
2024-06-11 03:18:06 -0300

    Adicionados dados de metodos e tostring

---
+++ SegundoProblemaExemplo/SegundoProblemaExemplo/Produto.cs
@@ -0,0 +1,36 @@
+﻿
+namespace SegundoProblemaExemplo
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValotTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValotTotalEmEstoque().ToString("F2");
+        }
+    }
+}
