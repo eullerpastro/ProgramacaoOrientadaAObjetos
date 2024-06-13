538b45867e7f2dbaa394bb8660de020258218122
Euller Pastro <eullerpastro0@gmail.com>
2024-06-13 20:46:45 -0300

    Exercicio de fixacao sobre calculo de retangulo

---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a Largura e a altura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine());
+            r.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("AREA = " + r.Area());
+            Console.WriteLine("PERIMETRO = " + r.Perimetro());
+            Console.WriteLine("DIAGONAL = " + r.Diagonal());
+        }
+    }
+}
