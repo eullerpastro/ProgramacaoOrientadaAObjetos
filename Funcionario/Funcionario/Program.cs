26071d71469134e9171f0310ac3340a69be1d1d5
Euller Pastro <eullerpastro0@gmail.com>
2024-06-13 21:17:09 -0300

    Exercicio de fixacao sobre o salario de um funcionario

---
+++ Funcionario/Funcionario/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Funcionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + f);
+
+            Console.WriteLine();
+            Console.WriteLine("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine());
+            f.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + f);
+        }
+    }
+}
