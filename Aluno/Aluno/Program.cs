58ff9eca5f14267995f7470e00a9c83f7946be0f
Euller Pastro <eullerpastro0@gmail.com>
2024-06-13 21:38:55 -0300

    Exercicio de fixacao sobre notas de alunos

---
+++ Aluno/Aluno/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+            Console.WriteLine("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            a.Nota1 = double.Parse(Console.ReadLine());
+            a.Nota2 = double.Parse(Console.ReadLine());
+            a.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("NOTA FINAL = " + a.NotaFinal().ToString("F2"));
+
+            if (a.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                + a.NotaFinal().ToString("F2")
+                + " PONTOS");
+            }
+        }
+    }
+}
