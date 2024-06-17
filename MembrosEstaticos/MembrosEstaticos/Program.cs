a6137ab698fd7c5e68030df4c18f14dfce338135
Euller Pastro <eullerpastro0@gmail.com>
2024-06-17 09:26:41 -0300

    Trabalhando com membros estaticos e static

---
+++ MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MembrosEstaticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2"));
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2"));
+        }
+    }
+}
