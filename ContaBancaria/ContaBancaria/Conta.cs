be39f27a384576d092058bcf81da4b9cc5bef690
Euller Pastro <eullerpastro0@gmail.com>
2024-07-01 08:42:02 -0300

    Exercicio

---
+++ ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    internal class Conta
+    {
+        public int Numero;
+        public string Nome;
+        public double Deposito;
+    }
+
+    public Conta()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return 
+    }
+
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+
+            Console.Write("Entre o número da conta: ");
+            c.Numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            c.Nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            c.Deposito = double.Parse(Console.ReadLine());
+        }
+    }
+}
