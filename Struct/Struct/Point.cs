2cbaf0c19b2c56837a6df98a649d3a77170252b5
Euller Pastro <eullerpastro0@gmail.com>
2024-07-10 08:27:04 -0300

    Struct

---
+++ Struct/Struct/Point.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Struct
+{
+    internal struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
