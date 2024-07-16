a4663513b8f165b9ffba9b246c4d4238c5b29827
Euller Pastro <eullerpastro0@gmail.com>
2024-07-16 08:33:21 -0300

    Parans

---
+++ Paramans/Paramans/Calculator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Paramans
+{
+    namespace Paramans
+    {
+        class Calculator
+        {
+            public static int Sum(params int[] numbers)
+            {
+                int sum = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sum += numbers[i];
+                }
+                return sum;
+            }
+        }
+    }
+}
