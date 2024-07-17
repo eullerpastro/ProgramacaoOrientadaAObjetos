9052f478715e7f249b214a16e9e35bdf9d0968f0
Euller Pastro <eullerpastro0@gmail.com>
2024-07-17 08:22:57 -0300

    Usando ref e out

---
+++ RefEOut/RefEOut/Calculator.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace RefEOut
+{
+    class Calculator
+    {
+        public static void Triple(int  origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ RefEOut/RefEOut/Program.cs
@@ -0,0 +1,16 @@
+﻿using RefEOut;
+using System;
+
+namespace RefEOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
