2027c58972133b7501766deb416da1eb621a3396
Euller Pastro <eullerpastro0@gmail.com>
2024-07-17 08:43:55 -0300

    Aprendendo list

---
+++ List/List/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            List<string> list2 = new List<string> { "Maria", "Alex", };            
+        }
+    }
+}
