35504758aa6d5498679d3b8ee13e9fe8e8c590f2
Euller Pastro <eullerpastro0@gmail.com>
2024-08-19 08:55:02 -0300

    Enum

---
+++ Enum/Enum/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Enum.Entities.Enums
+{
+    internal enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Prcessing = 1,
+        Shipped = 2,
+        Delivered = 3,
+    }
+}
---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,25 @@
+﻿using Enum.Entities;
+using Enum.Entities.Enums;
+using System;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();       
+
+            Console.WriteLine(txt);            
+        }
+    }
+}
