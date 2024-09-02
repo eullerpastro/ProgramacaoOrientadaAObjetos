c2efdd56224363d290fa08dadeaddcfd639b922c
Euller Pastro <eullerpastro0@gmail.com>
2024-09-02 09:02:39 -0300

    Exercicio sobre heranca e polimorfismo

---
+++ HerancaEPolimorfismo/HerancaEPolimorfismo/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace HerancaEPolimorfismo.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHourdouble, double additionalCharge)
+            : base (name, hours, valuePerHourdouble)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
